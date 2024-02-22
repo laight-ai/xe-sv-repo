40c70815a985a410db2b05899a82f9cfb303cb2c
singleview-co-kr <w9721066@gmail.com>
2024-02-22 12:32:16 +0900

    Commit to #20
    Add a feature - transfer a page with no document
    
    modified:   addons/svperm_move/svperm_move.addon.php
    modified:   modules/svperm_move/svperm_move.model.php

--- addons/svperm_move/svperm_move.addon.php
+++ addons/svperm_move/svperm_move.addon.php
@@ -13,25 +13,31 @@ if(!defined('__XE__'))
 if(!defined( '__ZBXE__' ) )
 	exit();
 
-//$sMid = Context::get('mid');
-$nDocumentSrl = Context::get('document_srl');
-//if( strlen($sMid) && intval($nDocumentSrl) )
-if( intval($nDocumentSrl) )
-{
-	$s301TargetUrl = null;
-	if(getClass('svperm_move'))
-	{
-		$oSvperm_moveModel = getModel('svperm_move');
-		//$s301TargetUrl = $oSvperm_moveModel->get301Url( $sMid, $nDocumentSrl );
-		$s301TargetUrl = $oSvperm_moveModel->get301Url( $nDocumentSrl );
-	}
+if(!getClass('svperm_move'))
+	exit();
 
-	if(!is_null($s301TargetUrl))
-	{
-		header('HTTP/1.1 301 Moved Permanently');
-		header('Location: '.$s301TargetUrl);
-		exit;
-	}
+$oSvperm_moveModel = getModel('svperm_move');
+$s301TargetUrl = null;
+$nDocumentSrl = Context::get('document_srl');
+$sMid = Context::get('mid');
+if( intval($nDocumentSrl) )  // document case
+{
+	$s301TargetUrl = $oSvperm_moveModel->get301UrlByDocSrl( $nDocumentSrl );
 }
+elseif( strlen($sMid) )  // page case
+{
+	$s301TargetUrl = $oSvperm_moveModel->get301UrlByMid( $sMid );
+}
+unset($oSvperm_moveModel);
+
+if(!is_null($s301TargetUrl))
+{
+	header('HTTP/1.1 301 Moved Permanently');
+	header('Location: '.$s301TargetUrl);
+	exit;
+}
+
+
+
 /* End of file perm_move.addon.php */
 /* Location: ./addons/perm_move/perm_move.addon.php */
--- modules/svperm_move/svperm_move.model.php
+++ modules/svperm_move/svperm_move.model.php
@@ -10,23 +10,41 @@ class svperm_moveModel extends svperm_move
  * @brief 
  * @return
  */
-	//public function get301Url($sMid, $nDocumentSrl)
-	public function get301Url($nDocumentSrl)
+	public function get301UrlByDocSrl($nDocumentSrl)
 	{
 		$oDbParam = new stdClass();
-		//$oDbParam->mid = trim($sMid);
 		$oDbParam->document_srl = intval($nDocumentSrl);
 		$oRst = executeQuery('svperm_move.get301Url', $oDbParam);
 		unset($oDbParam);
-		
+		$this->_insertLog($oRst->data->target_url);
+		return $oRst->data->target_url;
+	}
+/**
+ * @brief 
+ * @return
+ */
+	public function get301UrlByMid($sMid)
+	{
+		$oDbParam = new stdClass();
+		$oDbParam->mid = trim($sMid);
+		$oDbParam->document_srl = 0;
+		$oRst = executeQuery('svperm_move.get301Url', $oDbParam);
+		unset($oDbParam);
+		$this->_insertLog($oRst->data->target_url);
+		return $oRst->data->target_url;
+	}
+/**
+ * @brief 
+ * @return
+ */
+	private function _insertLog($sTargetUrl)
+	{
 		$oDbParam = new stdClass();
 		$oDbParam->http_referer = trim($_SERVER['HTTP_REFERER']);
 		$oDbParam->request_uri = trim($_SERVER['REQUEST_URI']);
-		$oDbParam->target_url = $oRst->data->target_url;
+		$oDbParam->target_url = $sTargetUrl;
 		$oInsertRst = executeQuery('svperm_move.insert301Log', $oDbParam);
 		unset($oDbParam);
-
-		return $oRst->data->target_url;
 	}
 }
 /* End of file svperm_move.model.php */
